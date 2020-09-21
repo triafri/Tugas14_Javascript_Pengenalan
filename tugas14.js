eee0dae6f7db8084122c857cf08a9d2f5dff6a95
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-21 09:22:10 +0700

    Add files via upload

---
+++ tugas14.js
@@ -0,0 +1,12 @@
+var bagi = 37/23
+
+console.log(bagi)
+
+console.log("Perintah math round")
+console.log(Math.round(bagi))
+
+console.log("Perintah math ceil")
+console.log(Math.ceil(bagi))
+
+console.log("Perintah math floor")
+console.log(Math.floor(bagi))
